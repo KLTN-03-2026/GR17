f6740c1ddbd20dd2386d236892c634097b7f804e
vuson5668-cmd <vuson5668@gmail.com>
2026-04-10 20:50:20 +0700

    chore(brand): rename ui brand to son

--- FE/src/layout/components/TopRocker.vue
+++ FE/src/layout/components/TopRocker.vue
@@ -6,7 +6,7 @@
 								<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
 							</div>
 							<div class="">
-								<h4 class="logo-text">Rocker</h4>
+								<h4 class="logo-text">Son</h4>
 							</div>
 						</div>
 						<div class="mobile-toggle-menu"><i class="bx bx-menu"></i></div>

3997cabf9300d87c1eb271ef63c305840d38a312
vuson5668-cmd <vuson5668@gmail.com>
2026-04-10 20:10:56 +0700

    Revert "Merge pull request #1 from KLTN-03-2026/Son"
    This reverts commit 01f1f1cf77c4e8b56704c731d268e165884dd9bc, reversing
    changes made to 34d790a75d23be3507b3fbb032fe026ca7750f3b.

--- BE/routes/console.php
+++ BE/routes/console.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('doi-tac:email-bao-cao-thang')
-    ->monthlyOn(1, '07:00')
-    ->timezone('Asia/Ho_Chi_Minh')
-    ->withoutOverlapping();
--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,74 +1,18 @@
 <template>
-  <router-view v-slot="{ Component, route }">
-    <component :is="resolveLayout(route)">
-      <keep-alive :max="12">
-        <component
-          v-if="route.meta?.keepAlive"
-          :is="Component"
-          :key="route.path"
-        />
-      </keep-alive>
-
-      <Transition name="page-transition" mode="out-in">
-        <component
-          v-if="!route.meta?.keepAlive"
-          :is="Component"
-          :key="route.fullPath"
-        />
-      </Transition>
-    </component>
-  </router-view>
-  <AppDialogHost />
+  <component :is="layout">
+      <router-view></router-view>
+  </component>
 </template>
 
 <script>
-import AppDialogHost from "./components/Shared/AppDialogHost.vue";
-
+const default_layout = "default";
 export default {
-  name: "App",
-  components: {
-    AppDialogHost,
-  },
-  methods: {
-    resolveLayout(route) {
-      return route?.meta?.layout || "div";
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || default_layout) + "-layout";
     },
   },
 };
 </script>
-
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html, body {
-  height: 100%;
-  width: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-}
-
-#app {
-  height: 100%;
-  width: 100%;
-  min-height: 100vh;
-}
-
-.page-transition-enter-active,
-.page-transition-leave-active {
-  transition: opacity 0.25s cubic-bezier(0.25, 0.8, 0.25, 1), transform 0.25s cubic-bezier(0.25, 0.8, 0.25, 1);
-}
-
-.page-transition-enter-from {
-  opacity: 0;
-  transform: translateY(10px) scale(0.99);
-}
-
-.page-transition-leave-to {
-  opacity: 0;
-  transform: translateY(-10px) scale(0.99);
-}
 </style>

6fb172e6f81be70ad8bd15e0fd1dc529560ffb82
noname-0211 <nguyenthanhdat52@dtu.edu.vn>
2026-04-10 22:15:23 +0700

    feat: add tour and activity detail files

---
+++ BE/app/Http/Requests/UpdateTourRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateTourRequest extends BaseRequest
+{
+    public function rules(): array
+    {
+        return [
+            'ten_tour' => 'sometimes|required|string|max:255',
+            'mo_ta' => 'nullable|string',
+            'hinh_anh' => 'nullable|string',
+            'so_tien' => 'nullable|numeric',
+            'so_ngay' => 'nullable|integer',
+            'so_nguoi' => 'nullable|integer',
+            'ma_tag' => 'nullable|string'
+        ];
+    }
+}

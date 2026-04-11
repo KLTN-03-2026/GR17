1afd90265d016c5d513bae4370baefafa3eec903
Binn2906 <binxinhgai2003@gmail.com>
2026-04-11 14:13:48 +0700

    AnhDung: commit current GR17 FE changes

---
+++ FE/src/layout/master/LandingLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app-container">
+    <!-- Top Navigation Bar -->
+    <TopNavBar />
+
+    <!-- Main Content -->
+    <main id="main-content">
+      <slot></slot>
+    </main>
+
+    <!-- Footer -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+import TopNavBar from '../components/TopNavBar.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  name: 'LandingLayout',
+  components: {
+    TopNavBar,
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+#app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  background-color: #ffffff;
+}
+
+#main-content {
+  flex: 1;
+  width: 100%;
+}
+</style>

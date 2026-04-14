6aebefec8190de1ebd84af6c96d2507d11f7dab1
Binn2906 <binxinhgai2003@gmail.com>
2026-04-14 21:39:36 +0700

    Update DichVuDiaDiemController and routes - April 14, 2026

--- BE/app/Http/Controllers/DichVuDiaDiemController.php
+++ BE/app/Http/Controllers/DichVuDiaDiemController.php
@@ -9,7 +9,15 @@ use Illuminate\Http\Request;
 
 class DichVuDiaDiemController extends Controller
 {
+    public function index()
+    {
+        $dich_vu = DichVuDiaDiem::with('diaDiem')->get();
 
+        return response()->json([
+            'success' => true,
+            'data' => $dich_vu
+        ]);
+    }
 
     public function store(StoreDichVuDiaDiemRequest $request)
     {
@@ -63,6 +71,32 @@ class DichVuDiaDiemController extends Controller
                 'success' => true,
                 'message' => 'Cập nhật thành công',
                 'data' => $dich_vu
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Lỗi: ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroy($ma_dich_vu)
+    {
+        $dich_vu = DichVuDiaDiem::find($ma_dich_vu);
+
+        if (!$dich_vu) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Dịch vụ không tồn tại'
+            ], 404);
+        }
+
+        try {
+            $dich_vu->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Xóa thành công'
             ]);
         } catch (\Exception $e) {
             return response()->json([

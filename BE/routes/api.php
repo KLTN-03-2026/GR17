f3b44e2226dd7e120235421daf47df494305d762
Binn2906 <binxinhgai2003@gmail.com>
2026-04-14 21:42:11 +0700

    Resolve merge conflict in routes/api.php - April 14, 2026

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -5,11 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TourController;
 use App\Http\Controllers\HoaDonController;
 use App\Http\Controllers\ChiTietTourController;
-<<<<<<< HEAD
 use App\Http\Controllers\DichVuDiaDiemController;
-=======
-
->>>>>>> 367e6940a2e6ebc1215da8dda8df04e1d1ff7107
+use App\Http\Controllers\NhomController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -22,10 +19,12 @@ Route::prefix('admin')->group(function () {
 });
 
 Route::apiResource('chi-tiet-tour', ChiTietTourController::class);
-=======
+
+Route::apiResource('dich-vu-dia-diem', DichVuDiaDiemController::class);
+Route::get('dich-vu-dia-diem-all', [DichVuDiaDiemController::class, 'index']);
+
 Route::prefix('nhom')->group(function () {
     Route::get('/', [NhomController::class, 'index']);
     Route::get('/{id}', [NhomController::class, 'show']);
     Route::post('/', [NhomController::class, 'store']);
 });
->>>>>>> 367e6940a2e6ebc1215da8dda8df04e1d1ff7107
